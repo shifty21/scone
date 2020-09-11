c2dd221c525045065818704a9270ee7490046c29
shifty21 <yateenderk@gmail.com>
2020-09-11 18:36:20 +0200

    consul dockerfile, run.sh for registering session

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ ADD . / /root/go/src/github.com/shifty21/scone/
 RUN apk add make vim git curl go musl-utils busybox-extras
 RUN chown root /usr/sbin/vault
 RUN mv /usr/local/bin/scone-gccgo /usr/local/bin/gccgo
+EXPOSE 8500 8600 80 8600/udp
--- resources/run.sh
+++ resources/run.sh
@@ -3,3 +3,6 @@
 cd /resources/ && curl -k -s --cert conf/client.crt --key conf/client-key.key --data-binary @session_vault.yml -X POST https://$SCONE_CAS_ADDR:8081/session
 #register vault_initializer
 cd /resources/ && curl -k -s --cert conf/client_back.crt --key conf/client-key_back.key --data-binary @session_vault_init.yml -X POST https://$SCONE_CAS_ADDR:8081/session
+#vault 
+#cd /resources/ && vault server -config=config.hcl
+#SCONE_CONFIG_ID=vault_demo/vault vault server -config=config.hcl
