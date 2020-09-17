1589cf727f76c3a2855f9711a893b140c60f1f81
shifty21 <yateenderk@gmail.com>
2020-09-17 12:11:20 +0200

    comment update for consul-template

--- gccgo/gccgo.sh
+++ gccgo/gccgo.sh
@@ -4,5 +4,5 @@ source /root/.profile
 #add // +build !gccgo to the files giving reflect.
 #change type_map.go file in reflect2 package, which will pe shown by build error
 /usr/local/go/bin/go build -compiler gccgo -o vault_binary -v 
-#add gccgo compiler flag in consul-template and run make dev
+#add gccgo compiler flag and remove CGO_ENABLED=0 in consul-template Makefile's dev function and run make dev
 # clone scone repo https://github.com/shifty21/scone to build vault initilizer, run make -i in root dir
